ec409d8608b148408f1909c904f0a0a7db593b9c
CindyMisoi <crmisoi2k@gmail.com>
2022-11-25 00:19:01 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+// // const add = (parameter1, parameter2) => {
+// //    return parameter1 + parameter2;
+// // }
+// // console.log(add(2,3));
+
+// // can be
+
+// const add = (param1, param2)=> param1+param2;
+// console.log(add(2,3));
+
+// //with one parameter
+// const sum = x => x+2;
+// console.log(sum(5));
+
+// //arrow functions to iterate
+// const nums = [1,2,3];
+// const squares = nums.map(x =>  x ** 2);
+// console.log(squares);
+// console.log(nums);
+
+const divide = function(){
+return 2000 / 100;
+}
+divide();
+
+const square = x =>  x * x;
+square(5);
+
+const add = (a,b) => a  + b;
+add(a,b);
+  
